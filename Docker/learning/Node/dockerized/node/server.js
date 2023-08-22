23a3002a322296b216ffc22b07cbca7151add27b
princewillopah <princewillopah@gmail.com>
2023-08-22 14:10:34 +0000

    from work

--- Docker/learning/Node/dockerized/node/server.js
+++ Docker/learning/Node/dockerized/node/server.js
@@ -1,8 +1,15 @@
 const express = require('express');
+
+const mongoose = require('mongoose');
 const path = require('path');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+mongoose
+.connect("mongodb://princewillopah:PRINCEWILL1980@mongo:27017/?authSource=admin")
+.then(()=> console.log("successful connctedto DB"))
+.catch((e)=>console.log(e))
 
 app.use(express.static(path.join(__dirname)));
 
